4c0b4afcae343080e5a5601f0015b32f6d330870
A22PedroGM <a22pedrogm@a21eql17.sanclemente.local>
2023-11-24 14:27:37 +0100

    Menu lateral con fallos, creación de fragmentos sin funcionalidad y grafico de navegación

--- app/src/main/java/com/example/pmdm_dam_proyecto/MainActivity.kt
+++ app/src/main/java/com/example/pmdm_dam_proyecto/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.pmdm_dam_proyecto
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val drawerLayout =
+            findViewById<androidx.drawerlayout.widget.DrawerLayout>(R.id.drawer_layout)
+        val builderApp = AppBarConfiguration.Builder(navController.graph)
+        builderApp.setOpenableLayout(drawerLayout)
+
     }
 }
+
