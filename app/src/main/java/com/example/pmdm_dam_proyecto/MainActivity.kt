e9b77e11b6588ca457887d59d14b5c6c7c574eca
A22PedroGM <a22pedrogm@a21eql17.sanclemente.local>
2023-11-27 14:31:54 +0100

    Iconos generales del pong corrección de errores al crear el menu lateral

--- app/src/main/java/com/example/pmdm_dam_proyecto/MainActivity.kt
+++ app/src/main/java/com/example/pmdm_dam_proyecto/MainActivity.kt
@@ -1,25 +1,46 @@
 package com.example.pmdm_dam_proyecto
 
 import android.os.Bundle
-import android.view.Menu
-import androidx.appcompat.app.ActionBarDrawerToggle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        val drawerLayout =
-            findViewById<androidx.drawerlayout.widget.DrawerLayout>(R.id.drawer_layout)
-        val builderApp = AppBarConfiguration.Builder(navController.graph)
-        builderApp.setOpenableLayout(drawerLayout)
 
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        // Inicializar las variables de navegación después de setContentView
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val builderApp = AppBarConfiguration.Builder(navController.graph)
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        builderApp.setOpenableLayout(drawerLayout)
+        appBarConfiguration = builderApp.build()
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, navController) || super.onOptionsItemSelected(item)
     }
 }
 
